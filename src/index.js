7cf6048e5100df2d15921360e9d181107a005ec7
Bypupil <bypupil@outlook.com>
2018-06-10 01:37:45 +0800

    v0.0.1初次提交

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert('测试');
+class FastCache {
+	constructor() {
+		this.list = {}
+	}
+	set(key, value) {
+		this.list[key] = value
+	}
+	get(key) {
+		return this.list[key]
+	}
+}
+
+window.FastCache = FastCache
